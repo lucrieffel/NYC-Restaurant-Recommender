212db6b8c8b4ff37e7299fbb2585c18b523011f6
Luc Rieffel <65513046+lucrieffel@users.noreply.github.com>
2024-08-01 15:01:41 +0700

    outline for recommendations page

---
+++ pages/recommendations.tsx
@@ -0,0 +1,6 @@
+//Path: /Users/lucrieffel/Desktop/CPSC_458/MP4_YelpRec/mp4_yelprec/pages/recommendations.tsx
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import SearchBar from '../components/SearchBar'; //use searchbar to filter recommendations by cuisine
+
